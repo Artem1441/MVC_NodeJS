edd828b76b92a50ea492e591aff70d10d912f93b
Artem1441 <74830063+Artem1441@users.noreply.github.com>
2021-12-31 10:19:17 +0500

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,12 +1,6 @@
 const firebase = require("firebase");
 const firebaseConfig = {
-  apiKey: "AIzaSyDA9vCDji0sYteNJOIMV81DyGHuaorN5hQ",
-  authDomain: "fern-46a78.firebaseapp.com",
-  projectId: "fern-46a78",
-  storageBucket: "fern-46a78.appspot.com",
-  messagingSenderId: "768371104160",
-  appId: "1:768371104160:web:5c888e64ac35cfc37eecc5",
-  measurementId: "G-RCJ1MWMLT9",
+//   firebase connection here
 };
 firebase.initializeApp(firebaseConfig);
 
